1b040eafe008739c8923af283e073a4ba8751ba5
Karl Mellinger <mellinka@oregonstate.edu>
2026-03-12 23:15:42 -0700

    Longest Repeating Character Replacement.

---
+++ sliding-window/longest-repeating-character-replacement.py
@@ -0,0 +1,30 @@
+"""
+Name: Longest Repeating Character Replacement
+Difficulty: Medium
+Mastered: No
+
+Time: O(n)
+Space: O(m), where m is the number of unique chars.
+Notes: Sliding window, keep track of most frequent character.
+"""
+from collections import defaultdict
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        left = 0
+        max_freq = 0
+        longest = 0
+
+        for right in range(len(s)):
+            count[s[right]] += 1
+            max_freq = max(max_freq, count[s[right]])
+
+            while (right - left + 1) - max_freq > k:
+                count[s[left]] -= 1
+                left += 1
+
+            longest = max(longest, right - left + 1)
+
+        return longest
