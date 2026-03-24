b7db8ab6dc70f899cdd5b05b40f655c8bcbf0b08
Karl Mellinger <mellinka@oregonstate.edu>
2026-03-23 22:40:59 -0700

    Valid Parentheses.

---
+++ stack/valid-parentheses.py
@@ -0,0 +1,24 @@
+"""
+Name: Valid Parentheses
+Difficulty: Easy
+Mastered: Yes
+
+Time: O(n)
+Space: O(n)
+Notes: Stack
+"""
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        paren_pairs = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+        for c in s:
+            if c in paren_pairs.keys():
+                stack.append(c)
+            else:
+                if not stack or paren_pairs[stack[-1]] != c:
+                    return False
+                else:
+                    stack.pop()
+        return True if not stack else False
